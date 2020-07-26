7653e1da5a856f74ef30c332f94649a43843a9b6
Kelly Shen <kellygshen@gmail.com>
2020-07-26 05:05:32 +0000

    stuff

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("here we go again!");
-    System.out.println("i am so confused??")
+    System.out.println("i am so confused??");
+    System.out.println("this better work");
   }
 }
